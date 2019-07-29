masterking32/TrinityCore

commit c71e36acaea9c04e63dcba9a39b5a6a76e3cac75

Scripts/Blood Furnace: Broggok improvements (#23300)
* 23285 Fixed lever respawn and future encounters after it respawns.

* Removed unnecessary additional container.
Made lever not to despawn and respawn but rather change flags only.

* Made one conditional branch easier to debug.

src/server/scripts/Outland/HellfireCitadel/BloodFurnace/instance_blood_furnace.cpp
@@ -33,6 +33,18 @@ DoorData const doorData[] =
     { 0,                 0,                        DOOR_TYPE_ROOM } // END
 };
 
+ObjectData const creatureData[] =
+{
+    { NPC_BROGGOK,             DATA_BROGGOK             },
+    { 0,                       0                        } // END
+};
+
+ObjectData const gameObjectData[] =
+{
+    { GO_BROGGOK_LEVER,      DATA_BROGGOK_LEVER },
+    { 0,                     0                  } //END
+};
+
 class instance_blood_furnace : public InstanceMapScript
 {
     public:
@@ -45,6 +57,7 @@ class instance_blood_furnace : public InstanceMapScript
                 SetHeaders(DataHeader);
                 SetBossNumber(EncounterCount);
                 LoadDoorData(doorData);
+                LoadObjectData(creatureData, gameObjectData);
 
                 PrisonerCounter5        = 0;
                 PrisonerCounter6        = 0;
@@ -54,6 +67,8 @@ class instance_blood_furnace : public InstanceMapScript
 
             void OnCreatureCreate(Creature* creature) override
             {
+                InstanceScript::OnCreatureCreate(creature);
+
                 switch (creature->GetEntry())
                 {
                     case NPC_THE_MAKER:
@@ -153,8 +168,6 @@ class instance_blood_furnace : public InstanceMapScript
                                 break;
                             case NOT_STARTED:
                                 ResetPrisons();
-                                if (GameObject* lever = instance->GetGameObject(BroggokLeverGUID))
-                                    lever->Respawn();
                                 break;
                             default:
                                 break;
@@ -169,28 +182,35 @@ class instance_blood_furnace : public InstanceMapScript
 
             void ResetPrisons()
             {
-                PrisonerCounter5 = PrisonersCell5.size();
                 ResetPrisoners(PrisonersCell5);
+                PrisonerCounter5 = PrisonersCell5.size();
                 HandleGameObject(PrisonCellGUIDs[DATA_PRISON_CELL5 - DATA_PRISON_CELL1], false);
 
-                PrisonerCounter6 = PrisonersCell6.size();
                 ResetPrisoners(PrisonersCell6);
+                PrisonerCounter6 = PrisonersCell6.size();
                 HandleGameObject(PrisonCellGUIDs[DATA_PRISON_CELL6 - DATA_PRISON_CELL1], false);
 
-                PrisonerCounter7 = PrisonersCell7.size();
                 ResetPrisoners(PrisonersCell7);
+                PrisonerCounter7 = PrisonersCell7.size();
                 HandleGameObject(PrisonCellGUIDs[DATA_PRISON_CELL7 - DATA_PRISON_CELL1], false);
 
-                PrisonerCounter8 = PrisonersCell8.size();
                 ResetPrisoners(PrisonersCell8);
+                PrisonerCounter8 = PrisonersCell8.size();
                 HandleGameObject(PrisonCellGUIDs[DATA_PRISON_CELL8 - DATA_PRISON_CELL1], false);
             }
 
-            void ResetPrisoners(GuidSet const& prisoners)
+            void ResetPrisoners(GuidSet& prisoners)
             {
-                for (GuidSet::const_iterator i = prisoners.begin(); i != prisoners.end(); ++i)
-                    if (Creature* prisoner = instance->GetCreature(*i))
+                for (GuidSet::const_iterator i = prisoners.begin(); i != prisoners.end();)
+                    if (Creature * prisoner = instance->GetCreature(*i))
+                    {
+                        if (!prisoner->IsAlive())
+                            i = prisoners.erase(i);
+                        else
+                            ++i;
+
                         ResetPrisoner(prisoner);
+                    }
             }
 
             void ResetPrisoner(Creature* prisoner)

src/server/scripts/Outland/HellfireCitadel/BloodFurnace/boss_broggok.cpp
@@ -58,6 +58,13 @@ class boss_broggok : public CreatureScript
             void Reset() override
             {
                 _Reset();
+
+                if (GameObject * lever = instance->GetGameObject(DATA_BROGGOK_LEVER))
+                {
+                    lever->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_NOT_SELECTABLE | GO_FLAG_IN_USE);
+                    lever->SetGoState(GO_STATE_READY);
+                }
+
                 DoAction(ACTION_RESET_BROGGOK);
             }
 
@@ -144,12 +151,14 @@ class go_broggok_lever : public GameObjectScript
                 if (instance->GetBossState(DATA_BROGGOK) != DONE && instance->GetBossState(DATA_BROGGOK) != IN_PROGRESS)
                 {
                     instance->SetBossState(DATA_BROGGOK, IN_PROGRESS);
-                    if (Creature* broggok = ObjectAccessor::GetCreature(*me, instance->GetGuidData(DATA_BROGGOK)))
+                    if (Creature* broggok = instance->GetCreature(DATA_BROGGOK))
                         broggok->AI()->DoAction(ACTION_PREPARE_BROGGOK);
                 }
 
-                me->UseDoorOrButton();
-                return false;
+                me->SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_NOT_SELECTABLE | GO_FLAG_IN_USE);
+                me->SetGoState(GO_STATE_ACTIVE);
+
+                return true;
             }
         };
 

commit 07fdae8219e9929fe65bd9756d053698e94d6284

Scripts/Blood Furnace: Fix infinite loop in Broggok encounter
Fix infinite loop in Broggok encounter caused by https://github.com/TrinityCore/TrinityCore/pull/23300

src/server/scripts/Outland/HellfireCitadel/BloodFurnace/instance_blood_furnace.cpp
@@ -202,6 +202,7 @@ class instance_blood_furnace : public InstanceMapScript
             void ResetPrisoners(GuidSet& prisoners)
             {
                 for (GuidSet::const_iterator i = prisoners.begin(); i != prisoners.end();)
+                {
                     if (Creature * prisoner = instance->GetCreature(*i))
                     {
                         if (!prisoner->IsAlive())
@@ -211,6 +212,9 @@ class instance_blood_furnace : public InstanceMapScript
 
                         ResetPrisoner(prisoner);
                     }
+                    else
+                        ++i;
+                }
             }
 
             void ResetPrisoner(Creature* prisoner)